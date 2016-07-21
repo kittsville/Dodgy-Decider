c936a952a36efb8d068205ba780bcd8afac557d3
Kit <kittsville@googlemail.com>
2016-07-22 00:20:08 +0100

    Added chooseNumber method
    Remove duplicate code from chooseElement

--- src/Decider.php
+++ src/Decider.php
@@ -35,28 +35,45 @@ class Decider {
     }
     
     /**
-     * Chooses a value from an array
+     * Chooses number between given range or between 0 and given number
      *
-     * @throws LogicException If array has no elements
+     * @throws LogicException If given numeric range is invalid
      *
-     * @param  Array  $array Array of choices to choose an element from
+     * @param  int    $lower OPTIONAL Lower bound
+     * @param  int    $upper Upper bound
      * @param  string $salt  OPTIONAL Salt to personalise choice
      *
-     * @return mixed Chosen array value
+     * @return int Chosen number between range
      */
-    public function chooseElement(Array $array, $salt = null)
+    public function chooseNumber($a, $b = null, $c = null)
     {
+        if ($b === null && $c === null) {
+            // If only $upper has been given
+            $lower = 0;
+            $upper = $a;
+            $salt  = null;
+        } else if ($c === null) {
+            // If the $upper and $lower are given
+            $lower = $a;
+            $upper = $b;
+            $salt  = null;
+        } else {
+            // If all three parameters are given
+            $lower = $a;
+            $upper = $b;
+            $salt  = $c;
+        }
+        
+        $num_choices = ($upper - $lower) + 1;
+        
+        if ($num_choices <= 0) {
+            throw LogicException('Upper bound must be greater than or equal to lower bound');
+        }
+        
         if ($salt !== null) {
             $salted_seed = hash('sha256', $this->seed . $salt);
         } else {
             $salted_seed = $this->seed;
-        }
-        
-        $choices     = array_keys($array);
-        $num_choices = count($choices);
-        
-        if ($num_choices === 0) {
-            throw LogicException('Array has no elements to choose one from');
         }
         
         // Uses 2 orders of magnitude to avoid clustering
@@ -73,6 +90,30 @@ class Decider {
         $hex_seed = hexdec(substr($salted_seed, 0, $hex_needed));
         
         $choice = intval(($hex_seed * $seed_multiplier) / $factor_divisor);
+        
+        return $lower + $choice;
+    }
+    
+    /**
+     * Chooses a value from an array
+     *
+     * @throws LogicException If array has no elements
+     *
+     * @param  Array  $array Array of choices to choose an element from
+     * @param  string $salt  OPTIONAL Salt to personalise choice
+     *
+     * @return mixed Chosen array value
+     */
+    public function chooseElement(Array $array, $salt = null)
+    {
+        $choices     = array_keys($array);
+        $num_choices = count($choices);
+        
+        if ($num_choices === 0) {
+            throw LogicException('Array has no elements to choose one from');
+        }
+        
+        $choice = $this->chooseNumber(0, $num_choices - 1, $salt);
         
         return $array[$choices[$choice]];
     }
